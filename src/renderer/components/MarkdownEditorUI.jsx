a31e76fe44985193e1c89ddfd35d1f1f2feec4c4
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-05-04 15:47:57 +0900

    Add Editor

--- src/renderer/components/MarkdownEditorUI.jsx
+++ src/renderer/components/MarkdownEditorUI.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Editor from "./Editor";
+import style from "./MarkdownEditorUI.css"
 
 export default class MarkdownEditorUI extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ""};
+        this.onChangeText = this.onChangeText.bind(this);
+    }
+
+    onChangeText(e) {
+        this.setState({text: e.target.value});
+    }
+
     render() {
+        console.log(style);
         return (
-            <div>
-                <h1>Hello, MarkdownEditor!</h1>
+            <div className={style.markdownEditor}>
+                <Editor
+                    className={style.editorArea}
+                    value={this.state.text}
+                    onChange={this.onChangeText}
+                />
             </div>
         );
     }
