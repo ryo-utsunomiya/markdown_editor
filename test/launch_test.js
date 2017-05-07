89ccafbfffe127c801df2b7c7fb7a483fecd0daa
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-05-07 14:30:08 +0900

    add mocha

--- test/launch_test.js
+++ test/launch_test.js
@@ -1,19 +1,20 @@
 const Application = require("spectron").Application;
 const electron = require("electron");
 const path = require("path");
+const assert = require("assert");
 
 const app = new Application({
     path: electron,
     args: [path.join(__dirname, "..")]
 });
 
-app.start()
-    .then(() => app.client.getWindowCount())
-    .then((count) => {
-        if (count === 1) {
-            console.log("success test:)");
-        } else {
-            console.log("failed test :(");
-        }
-        app.stop();
-    });
+describe("Test application launching", function () {
+    this.timeout(10000);
+    beforeEach(() => app.start());
+    afterEach(() => app.stop());
+
+    it("One window is displayed after application launch", function () {
+        return app.client.getWindowCount()
+            .then((count) => assert.equal(count, 1));
+    })
+});
